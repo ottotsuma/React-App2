69b6c18f805c800f3348777b83732576e5c8eaed
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2020-03-22 16:58:43 +0900

    Updates

---
+++ precache-manifest.754fd03933fe7ff870ea2cf0aee466d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3218e03be410626980a5353f361eff44",
+    "url": "/React-App2/index.html"
+  },
+  {
+    "revision": "fb3ae992aeedf5f8db47",
+    "url": "/React-App2/static/css/main.08a6197c.chunk.css"
+  },
+  {
+    "revision": "ec44888b181b253ce9a9",
+    "url": "/React-App2/static/js/2.b69314b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App2/static/js/2.b69314b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb3ae992aeedf5f8db47",
+    "url": "/React-App2/static/js/main.3e14f9fb.chunk.js"
+  },
+  {
+    "revision": "b990619ea1e3aa7c1e99",
+    "url": "/React-App2/static/js/runtime-main.b734f305.js"
+  }
+]);
